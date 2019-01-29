62248c3016af5f4271c14c216d039d5de95c4dc0
nevets615 <surj316@yahoo.com>
2019-01-29 18:57:50 -0500

    last push before stand up even more working

--- js/index.js
+++ js/index.js
@@ -11,59 +11,59 @@ console.log(e);
 
 
 const h2text = document.querySelector('h2');
-h2text.addEventListener('mouseDown', e => {
+h2text.addEventListener('mousedown', e => {
 e.target.innerHTML = "The mouse button is held down.";
 });
 
-const h4text = document.querySelector('h4');
-logoHeading.addEventListener('wheel', e => {
+// const h4text = document.querySelector('h4');
+// logoHeading.addEventListener('wheel', e => {
 
-e.target.style.fontSize = "20px";
+// e.target.style.fontSize = "20px";
 
-});
+// });
         
-const h4text = document.querySelector("h4");
-logoHeading.addEventListener("load", e => {
-e.Target.focus();
+// const h4text = document.querySelector("h4");
+// logoHeading.addEventListener("load", e => {
+// e.Target.focus();
 
-});
+// });
             
-const contentSection = document.querySelector('.content-section');
-logoHeading.addEventListener('blur', e => {
-    e.target.blur();
-});
+// const contentSection = document.querySelector('.content-section');
+// logoHeading.addEventListener('blur', e => {
+//     e.target.blur();
+// });
                 
-const window = document.querySelector('.window');
-window.addEventListener('resize', e => {
-let a = 0;
-let y = x += 2
-    e.target.innerHTML = y;
+// const window = document.querySelector('.window');
+// window.addEventListener('resize', e => {
+// let a = 0;
+// let y = a += 2;
+//     e.target.innerHTML = y;
     
-});
+// });
                     
-const inversecontent = document.querySelector('.content-section inverse-content');
-logoHeading.addEventListener('scroll', e => {
+// const inversecontent = document.querySelector('.content-section inverse-content');
+// logoHeading.addEventListener('scroll', e => {
 
-  e.target.innerHTML = "something happened";
+//   e.target.innerHTML = "something happened";
 
-});
+// });
                     
-const contentdestination = document.querySelector('.content-destination');
-logoHeading.addEventListener('select', e => {
+// const contentdestination = document.querySelector('.content-destination');
+// logoHeading.addEventListener('select', e => {
 
-    e.target.innerHTML = "something happened";
+//     e.target.innerHTML = "something happened";
 
 
-});
+// });
                             
-const logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener('dblclick', e => {
-e.tasrget.innerHTML = "I was double-clicked!";
+const contentsection1 = document.querySelector('.content-section');
+contentsection1.addEventListener('dblclick', e => {
+e.target.innerHTML = "I was double-clicked!";
 });
                                 
-const newbtn = document.querySelector('btn');
+const newbtn = document.querySelector('.btn');
 newbtn.addEventListener('click', e => {
-e.target.alert("Hello World!");
+e.target.innerHTML = "hello world";
       
 });
 
