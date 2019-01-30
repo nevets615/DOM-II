626c85a6351af66452187dfe70b0cac4612b8c73
nevets615 <surj316@yahoo.com>
2019-01-30 10:56:19 -0500

    just a couple of elements not working

--- js/index.js
+++ js/index.js
@@ -15,61 +15,68 @@ h2text.addEventListener('mousedown', e => {
 e.target.innerHTML = "The mouse button is held down.";
 });
 
-// const h4text = document.querySelector('h4');
-// logoHeading.addEventListener('wheel', e => {
+const h4text = document.querySelector('h4');
+h4text.addEventListener('wheel', e => {
 
-// e.target.style.fontSize = "20px";
+e.target.style.fontSize = "20px";
 
-// });
+});
         
-// const h4text = document.querySelector("h4");
-// logoHeading.addEventListener("load", e => {
-// e.Target.focus();
+const destination = document.querySelector(".destination");
+destination.addEventListener("load", e => {
+e.Target.innerHTML = "something happened.";
 
-// });
+});
             
-// const contentSection = document.querySelector('.content-section');
-// logoHeading.addEventListener('blur', e => {
-//     e.target.blur();
-// });
+const contentSection = document.querySelector('.content-section');
+contentSection.addEventListener('blur', e => {
+e.target.alert("something happened");
+});
                 
-// const window = document.querySelector('.window');
-// window.addEventListener('resize', e => {
-// let a = 0;
-// let y = a += 2;
-//     e.target.innerHTML = y;
+const windows = document.querySelector('.window');
+windows.addEventListener('resize', e => {
+let a = 0;
+let y = a += 2;
+    e.target.innerHTML = y;
     
-// });
+});
                     
 // const inversecontent = document.querySelector('.content-section inverse-content');
-// logoHeading.addEventListener('scroll', e => {
+// inversecontent.addEventListener('scroll', e => {
 
 //   e.target.innerHTML = "something happened";
 
 // });
                     
-// const contentdestination = document.querySelector('.content-destination');
-// logoHeading.addEventListener('select', e => {
+const contentdestination = document.querySelector('.content-destination');
+contentdestination.addEventListener('select', e => {
 
-//     e.target.innerHTML = "something happened";
+e.target.innerHTML = "something happened";
 
 
-// });
+});
                             
-const contentsection1 = document.querySelector('.content-section');
+const contentsection1 = document.querySelector('.logo-heading');
 contentsection1.addEventListener('dblclick', e => {
-e.target.innerHTML = "I was double-clicked!";
+console.log(`I was double-clicked!: ${e.target}`);
+
 });
                                 
 const newbtn = document.querySelector('.btn');
 newbtn.addEventListener('click', e => {
 e.target.innerHTML = "hello world";
-      
+e.stopPropagation(); 
 });
 
 const navbar = document.querySelector(".nav");
 navbar.addEventListener('click', e => {
     e.preventDefault();
 
+
+});
+
+const containerBtn = document.querySelector('.container');
+containerBtn.addEventListener('click', e => {
+console.log(`something happened ${e.target}`);
 
 });
